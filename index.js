ff7f151f56299c4a6def4ba91b43560611145903
Philip Olson <philip.olson@protonmail.ch>
2018-02-26 23:03:07 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "browser": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "prettier",
+  ],
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-unused-vars": [
+      "error",
+      {
+        "vars": "all",
+        "args": "all",
+        "ignoreRestSiblings": false,
+        "argsIgnorePattern": "^_"
+      }
+    ]
+  }
+};
